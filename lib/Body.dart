3e173672339a75c6d238fc5405f71a7c22e3f2c7
shanta15-4953 <shanta15-4953@diu.edu.bd>
2025-02-22 07:07:04 +0000

    last body.dart

--- lib/Body.dart
+++ lib/Body.dart
@@ -1,60 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/body.dart';
 
-void main() {
-  runApp(const Lab06AdvancedUI());
+class AppBody extends StatefulWidget {
+  const AppBody({super.key});
+
+  @override
+  State<AppBody> createState() => _AppBodyState();
 }
 
-/*
-  NAVIGATION
-  1. Drawer
-  2. Bottom Nav
-  3. Tab
-*/
+class _AppBodyState extends State<AppBody> {
+  int selectedIndex = 0;
 
-class Lab06AdvancedUI extends StatelessWidget {
-  const Lab06AdvancedUI({super.key});
+  final List<Widget> pages = [
+    Text("Home"),
+    Text("Search From Bottom Nav"),
+    Text("Settings"),
+  ];
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Lab06 Advanced UI',
-      home: Scaffold(
-        drawer: Drawer(
-          backgroundColor: const Color.fromARGB(255, 242, 245, 247),
-          child: ListView(
-            children: [
-              Container(
-                height: 80,
-                child: DrawerHeader(
-                  child: Row(
-                    children: [
-                      Icon(Icons.people_alt_rounded),
-                      Text("Contacts"),
-                    ],
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.mobile_screen_share),
-                title: Text("Device"),
-                trailing: Text("Anything"),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: Icon(Icons.mobile_screen_share),
-                title: Text("Contacts"),
-                trailing: Text("Anything"),
-                onTap: () {},
-              ),
-            ],
+    return Scaffold(
+      body: pages[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onItemTapped,
+        currentIndex: selectedIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
           ),
-        ),
-        appBar: AppBar(
-          title: const Text('Lab06 Advanced UI'),
-          backgroundColor: Colors.amber,
-        ),
-        body: AppBody(),
+        ],
       ),
     );
   }
