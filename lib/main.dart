dc7403653aa0f03a4a1ae5fdf27675b114c18b1d
shanta15-4953 <shanta15-4953@diu.edu.bd>
2025-02-22 05:35:02 +0000

    image 2nd

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,36 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Lab06());
+  runApp(const Lab06AdvancedUI());
 }
 
-class Lab06 extends StatelessWidget {
+class Lab06AdvancedUI extends StatelessWidget {
+  const Lab06AdvancedUI({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: "Lab O6 Advanced UI",
       home: Scaffold(
-        appBar: AppBar(title: Text("lab 06")),
-        body: Center( 
-          child: Image.network(
-            'https://diablodesign.eu/images/DiabloDesign/blog/online-photo-editor/v2-polarr.webp', 
-            loadingBuilder: (context, child, loadingProgress) {
-              if (loadingProgress == null) {
-                return child;
-              } else {
-                return Center(child: CircularProgressIndicator());
-              }
-            },
-            errorBuilder: (context, error, stackTrace) {
-              return Icon(Icons.error, size: 50, color: Colors.red);
-            },
+        appBar: AppBar(title: Text("Lab O6 Advanced UI")),
+        body: Center(
+          child: Column(
+            children: [
+              CachedNetworkImage(
+                imageUrl:
+                    "https://cdn.prod.website-files.com/61845f7929f5aa517ebab941/6440f9477c2a321f0dd6ab61_How%20Artificial%20Intelligence%20(AI)%20Is%20Used%20In%20Biometrics.jpg",
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Image.network(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmWru8q17zpOzzzT1s475ZS_8fOL1GS0teSw&s'),
+              ),
+              Expanded(
+                child: Image(
+                  image: NetworkImage(
+                      'https://fastly.picsum.photos/id/337/536/354.jpg?hmac=T7nJ4MjfcTPFuaaLRH_1zwqyOFzmldcV-f5abNr2MyE'),
+                ),
+              )
+            ],
           ),
         ),
       ),
